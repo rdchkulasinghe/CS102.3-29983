65e5b636c2522d12a5f85770af9416f89d88abae
rdchkulasinghe <140293329+rdchkulasinghe@users.noreply.github.com>
2023-07-27 18:53:44 +0530

    Add files via upload

---
+++ Practical-3-Q1/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+
+{
+   int n1,n2,max;
+   printf("Enter two numbers");
+   scanf("%d %d",&n1,&n2);
+
+   if(n1>n2)
+    max=n1;
+   else
+    max=n2;
+   printf("The highest is %d\n",max);
+}
